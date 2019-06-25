5d7503a2f1178d04883bc31cea36f35ff4b2c7c5
Yudi <madewahyudi22@gmail.com>
2019-06-25 15:35:14 +0800

    fix version typo

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import Main from './main';
 program
   .option('-b, --breaktime <breaktime>', 'Set Breaktime <default 5>', parseInt)
   .option('-m, --minutes <minutes>', 'Set Minutes <default 25>', parseInt)
-  .version('pomodoro-cli by yudi7ll version 0.1.0', '-v, --version')
+  .version('pomodoro-cli by yudi7ll version 1.0.0', '-v, --version')
   .parse(process.argv);
 
 const main = new Main(program.minutes || 25, program.breaktime || 5);
