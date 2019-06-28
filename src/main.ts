693ccfd4c5c992a8f3492bfeed3ac4ffa7063f51
Yudi <madewahyudi22@gmail.com>
2019-06-28 09:11:51 +0800

    session 4 crash fix

--- src/main.ts
+++ src/main.ts
@@ -20,55 +20,60 @@ export default class Main extends Timer {
   }
 
   toggleActiveMode = () => {
-	// toggle activeMode
 	this.activeMode = !this.activeMode;
-	// reset time
+
 	this.time.sec = 0;
-	// set min time && limit time to breakTime or activeTime
-	this.time.min = this.time.limit = this.activeMode ?
-	  this.activeTime : this.breakTime;
-	// if activeMode changed to then true session + 1
-	if (this.activeMode) this.session++;
-	// if current session >= 4 then triple the time limit
-	if (this.session >= 4) this.time.min *= 3;
+
+	// set min time & limit time to breakTime or activeTime
+	this.time.min = this.time.limit = this.activeMode
+	  ? this.activeTime
+	  : this.breakTime;
+
+	// if activeMode changed to true then session + 1
+	if (this.activeMode)
+	  this.session++;
+
+	// reset session
+	if (this.session > 4 && this.activeMode)
+	  this.session = 1;
+
+	// if not activeMode && current session >= 4
+	// then triple the time min
+	if (!this.activeMode && this.session >= 4)
+	  this.time.min = this.time.limit *= 3;
+
 	// send notification
 	this.notifySend();
   }
 
-  notifySend()
-  {
+  notifySend = () => {
 	// notification sound
-	var soundPath: string = [
+	let soundPath: string = [
 	  '/',
-	  // remove the last path
 	  path.resolve(__dirname).split('/').slice(0, -1).join('/'),
 	  '/sound/notif.ogg'
 	].join('');
-	// soundPath == /currentDirectory/sound/notif.ogg
+
+	// soundPath == /{currentDirectory}/sound/notif.ogg
 	exec('paplay ' + soundPath);
 
-	// // send notification
+	// send notification
 	let status = this.activeMode ? "Pomodoro " : "Break ";
 	exec(`notify-send "${status} ${this.session}, ${this.time.limit} Minutes"`);
   }
 
 
-  start()
-  {
+  start = () => {
 	// send notification first
 	this.notifySend();
 
 	setInterval(() => {
-
-	  // display time
 	  console.clear();
 	  Display(this.activeMode, this.session, this.time);
 
-	  // if the time reach the limit
 	  if (this.time.min <= 0 && this.time.sec <= 0)
 		this.toggleActiveMode();
 
-	  // decrease time
 	  this.decreaseTime();
 	}, 1000);
   }
