d65203dee050154ee629b8d6f56d372324bcda52
Yudi <madewahyudi22@gmail.com>
2019-06-28 09:12:28 +0800

    change -m options to -l --limit

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ import Main from './main';
 
 program
   .option('-b, --breaktime <breaktime>', 'Set Breaktime <default 5>', parseInt)
-  .option('-m, --minutes <minutes>', 'Set Minutes <default 25>', parseInt)
+  .option('-l, --limit <minutes>', 'Set Minutes <default 25>', parseInt)
   .version('pomodoro-cli by yudi7ll version 1.0.0', '-v, --version')
   .parse(process.argv);
 
-const main = new Main(program.minutes || 25, program.breaktime || 5);
+const main = new Main(program.limit || 25, program.breaktime || 5);
 
 // start loop
 main.start();
